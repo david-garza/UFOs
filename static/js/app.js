5a1b0db0e9f65356d671c0d2491406e57b6c262f
david-garza <locodave@gmail.com>
2022-04-06 12:21:40 -0500

    Fixed tableBuild and added filter

--- static/js/app.js
+++ static/js/app.js
@@ -8,17 +8,42 @@ var tbody = d3.select("tbody");
 function buildTable(data){
     // Clear table
     tbody.html("");
-}
 
-data.forEach((dataRow) => {
+    // Function to add rows and populate rows with data
+    data.forEach((dataRow) => {
     // HTML table row tag
     let row = tbody.append("tr");
 
-    // HTML row item tag
-    Object.values(dataRow).forEach((val) => {
+        // HTML row item tag
+        Object.values(dataRow).forEach((val) => {
 
-        // Pulls text from the object and adds a td tag.
-        let cell = row.append("td");
-        cell.text(val);
+            // Pulls text from the object and adds a td tag.
+            let cell = row.append("td");
+            cell.text(val);
+        });
     });
-});
+
+}
+
+// Date that handles button click for data filtering
+function handleClick() {
+
+     // look for id="datetime"
+    let date = d3.select("#datetime").property("value");
+
+    // Default filter, whole data table
+    let filteredData = tableData;
+
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Build table using the filtered data
+    buildTable(filteredData);
+}
+
+// Click handling call to run handleClick
+d3.selectAll("#filter-btn").on("click",handleClick);
+
+// Default table for first load of page
+buildTable(tableData);
